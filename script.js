fd1eaa039eaca79dd389f6623a79b077141d7827
Utsav <utsavtiwari030@outlook.com>
2023-01-02 18:25:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+
+
+
+function sendemail(){
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "minatobots30@gmail.com",
+    Password : "Utsav",
+    To : 'minatobots30@gmail.com',
+    From : document.getElementById('ema').value,
+    Subject : "Enquiry sent",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
+
+
+
+var preload= document.getElementById("loading")
+function loade()
+{
+  preload.style.display = 'none';
+}
